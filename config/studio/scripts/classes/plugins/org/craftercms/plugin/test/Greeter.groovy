245d1d98e7e1a0adff7bcf6796c59d4ddb412009
admin admin <evaladmin@example.com>
2022-03-14 11:22:44 -0600

    Install plugin org.craftercms.plugin.test 3.0.4

---
+++ config/studio/scripts/classes/plugins/org/craftercms/plugin/test/Greeter.groovy
@@ -0,0 +1,9 @@
+package plugins.org.craftercms.plugin.test
+
+class Greeter {
+
+  def greet() {
+    "hello!"
+  }
+
+}
---
+++ scripts/controllers/plugins/org/craftercms/plugin/test/test.get.groovy
@@ -0,0 +1,1 @@
+return "/templates/plugins/org/craftercms/plugin/test/test-controller.ftl"
